8d62a49b9224495b9b97653a9a508b47ee3d4998
5010 FHS 5 <roboticsteam.fhs5@gmail.com>
2026-03-30 19:05:17 -0400

    rumble update

--- src/main/java/frc/robot/rebuilt/subsystems/DriverDisplay/HubStatus.java
+++ src/main/java/frc/robot/rebuilt/subsystems/DriverDisplay/HubStatus.java
@@ -1,9 +1,14 @@
 package frc.robot.rebuilt.subsystems.DriverDisplay;
 
+import static edu.wpi.first.units.Units.Seconds;
+
 import edu.wpi.first.units.measure.Time;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 // added import
 import frc.robot.rebuilt.HubTracker.Shift;
 import org.frc5010.common.arch.GenericSubsystem;
+import org.frc5010.common.sensors.Controller;
 import org.littletonrobotics.junction.Logger;
 
 public class HubStatus extends GenericSubsystem {
@@ -22,5 +27,13 @@ public class HubStatus extends GenericSubsystem {
     io.updateInputs(inputs);
 
     Logger.processInputs("HubStatus", inputs);
+  }
+
+  public void configureButtonBindings(Controller driver, Controller operator) {
+    Trigger rumble = new Trigger(() -> timeRemainingInCurrentShift.lte(Seconds.of(3)));
+    rumble
+        .and(new Trigger(() -> timeRemainingInCurrentShift.gte(Seconds.of(2.5))))
+        .whileTrue(Commands.runOnce(() -> driver.setRumble(0.5)))
+        .onFalse(Commands.runOnce(() -> driver.setRumble(0)));
   }
 }
