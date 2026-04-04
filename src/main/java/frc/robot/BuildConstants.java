c895ee365ac9fa44747c1ce884b66e44956c5e4d
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2026-04-04 08:46:13 -0400

    Adjust Current Limits and Spindexer Speed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 541;
-  public static final String GIT_SHA = "c866f9d058290034c025fef6d3c3ce120add73ea";
-  public static final String GIT_DATE = "2026-03-28 15:36:18 EDT";
+  public static final int GIT_REVISION = 545;
+  public static final String GIT_SHA = "4a9c5f850b4831c3fc54190b941900616c7f4914";
+  public static final String GIT_DATE = "2026-04-01 14:41:42 EDT";
   public static final String GIT_BRANCH = "Rebuilt";
-  public static final String BUILD_DATE = "2026-03-30 18:53:30 EDT";
-  public static final long BUILD_UNIX_TIME = 1774911210645L;
+  public static final String BUILD_DATE = "2026-04-03 18:24:27 EDT";
+  public static final long BUILD_UNIX_TIME = 1775255067439L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
