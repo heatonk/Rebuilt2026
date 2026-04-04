2d571bf1a339aa6c18ede3a7e28c60a7133c260c
5010 FHS 5 <roboticsteam.fhs5@gmail.com>
2026-04-04 09:46:11 -0400

    build works for me removed a comment inbetween static and normal imports

--- src/main/java/frc/robot/rebuilt/subsystems/DriverDisplay/HubStatus.java
+++ src/main/java/frc/robot/rebuilt/subsystems/DriverDisplay/HubStatus.java
@@ -2,13 +2,11 @@ package frc.robot.rebuilt.subsystems.DriverDisplay;
 
 import static edu.wpi.first.units.Units.Seconds;
 
-// added import
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import org.frc5010.common.arch.GenericSubsystem;
 import org.frc5010.common.sensors.Controller;
 import org.littletonrobotics.junction.Logger;
-
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class HubStatus extends GenericSubsystem {
   private HubStatusIO io = new HubStatusIOImpl();
@@ -17,8 +15,9 @@ public class HubStatus extends GenericSubsystem {
   public void configureButtonBindings(Controller driver, Controller operator) {
     Trigger rumble = new Trigger(() -> inputs.timeRemainingInCurrentShift.lte(Seconds.of(3)));
     rumble.and(() -> inputs.timeRemainingInCurrentShift.gt(Seconds.of(2.5)));
-    rumble.onTrue(Commands.runOnce(() -> driver.setRumble(0.5)));
-    rumble.onFalse(Commands.runOnce(() -> driver.setRumble(0)));
+    rumble
+        .onTrue(Commands.runOnce(() -> driver.setRumble(0.5)))
+        .onFalse(Commands.runOnce(() -> driver.setRumble(0)));
   }
 
   @Override
@@ -28,7 +27,4 @@ public class HubStatus extends GenericSubsystem {
 
     Logger.processInputs("HubStatus", inputs);
   }
-
-  
-  }
-
+}
