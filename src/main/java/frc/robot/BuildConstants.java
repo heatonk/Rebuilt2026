ad5cdef4da0c50a84d670bb16a12ebdfc57aa8b7
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2026-04-06 10:04:17 -0400

    Adjusted spindexer speeds and removed right camera

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 546;
-  public static final String GIT_SHA = "c895ee365ac9fa44747c1ce884b66e44956c5e4d";
-  public static final String GIT_DATE = "2026-04-04 08:46:13 EDT";
+  public static final int GIT_REVISION = 547;
+  public static final String GIT_SHA = "fdfd87ea7a1c3539858534277fad2ec252ceb2d4";
+  public static final String GIT_DATE = "2026-04-04 10:49:20 EDT";
   public static final String GIT_BRANCH = "Rebuilt";
-  public static final String BUILD_DATE = "2026-04-04 10:47:34 EDT";
-  public static final long BUILD_UNIX_TIME = 1775314054578L;
+  public static final String BUILD_DATE = "2026-04-04 16:10:15 EDT";
+  public static final long BUILD_UNIX_TIME = 1775333415282L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
