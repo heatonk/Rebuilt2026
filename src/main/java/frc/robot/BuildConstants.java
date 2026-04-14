2ccf0e884216e4875e7d8fdef27f0793d9dc0972
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2026-04-13 20:46:52 -0400

    1024 Changes
    Autos, no buzz, turret zero

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,21 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-<<<<<<< Updated upstream
-  public static final int GIT_REVISION = 548;
-  public static final String GIT_SHA = "ad5cdef4da0c50a84d670bb16a12ebdfc57aa8b7";
-  public static final String GIT_DATE = "2026-04-06 10:04:17 EDT";
+  public static final int GIT_REVISION = 560;
+  public static final String GIT_SHA = "5c032f9ebe4b72b073d6594a66b233b2a0dac368";
+  public static final String GIT_DATE = "2026-04-13 16:29:05 EDT";
   public static final String GIT_BRANCH = "Rebuilt";
-  public static final String BUILD_DATE = "2026-04-06 12:11:19 EDT";
-  public static final long BUILD_UNIX_TIME = 1775491879394L;
-=======
-  public static final int GIT_REVISION = 556;
-  public static final String GIT_SHA = "b24e41348a508b9e21497e500eddd1c80dcfd28c";
-  public static final String GIT_DATE = "2026-04-11 11:31:58 EDT";
-  public static final String GIT_BRANCH = "mariachi";
-  public static final String BUILD_DATE = "2026-04-11 12:30:43 EDT";
-  public static final long BUILD_UNIX_TIME = 1775925043494L;
->>>>>>> Stashed changes
+  public static final String BUILD_DATE = "2026-04-13 20:32:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1776126766195L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/rebuilt/commands/TestCommands.java
+++ src/main/java/frc/robot/rebuilt/commands/TestCommands.java
@@ -1,7 +1,5 @@
 package frc.robot.rebuilt.commands;
 
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.rebuilt.Constants;
 // import frc.robot.rebuilt.subsystems.Climb.Climb;
@@ -10,10 +8,7 @@ import frc.robot.rebuilt.subsystems.Launcher.Launcher;
 import frc.robot.rebuilt.subsystems.intake.Intake;
 import java.util.Map;
 import org.frc5010.common.arch.GenericSubsystem;
-import org.frc5010.common.config.ConfigConstants;
-import org.frc5010.common.drive.GenericDrivetrain;
 import org.frc5010.common.sensors.Controller;
-import org.frc5010.common.sensors.camera.QuestNavInterface;
 
 public class TestCommands {
 
@@ -53,8 +48,6 @@ public class TestCommands {
     controller
         .createAButton()
         .whileTrue(launcher.getFlyWheelSysIdCommand().finallyDo(() -> launcher.stopAllMotors()));
-
-    
 
     // Shot tuning command – hold Y button to enter tuning mode
     // controller
