efb98f39ec6a9ff5a0ce1c6610a5279386653cb7
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2026-04-15 16:41:09 -0400

    Changed Cameras

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,22 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-<<<<<<< mariachi
-  public static final int GIT_REVISION = 552;
-  public static final String GIT_SHA = "64cf856bf4be023ca2e009a9b9e6309505a6c7e6";
-  public static final String GIT_DATE = "2026-04-07 16:10:59 EDT";
-  public static final String GIT_BRANCH = "mariachi";
-  public static final String BUILD_DATE = "2026-04-10 18:39:09 EDT";
-  public static final long BUILD_UNIX_TIME = 1775860749297L;
-=======
-  public static final int GIT_REVISION = 582;
-  public static final String GIT_SHA = "14a17df4be080d0af1a797365a4475ca05b013f2";
-  public static final String GIT_DATE = "2026-04-13 16:49:54 EDT";
-  public static final String GIT_BRANCH = "improvement/turret-control-update";
-  public static final String BUILD_DATE = "2026-04-13 16:47:20 EDT";
-  public static final long BUILD_UNIX_TIME = 1776113240372L;
->>>>>>> Rebuilt
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 616;
+  public static final String GIT_SHA = "fee11cd4205c02436f499f3aca355b54ddd95eb1";
+  public static final String GIT_DATE = "2026-04-14 20:11:26 EDT";
+  public static final String GIT_BRANCH = "Rebuilt";
+  public static final String BUILD_DATE = "2026-04-15 16:37:22 EDT";
+  public static final long BUILD_UNIX_TIME = 1776285442446L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
