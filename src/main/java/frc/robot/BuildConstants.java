622233a67c876923bb13f48802cf8a5bbd8585cc
Aarush Jaiswal <78714918+saarushjaiswal@users.noreply.github.com>
2026-04-23 15:47:33 -0400

    new autos yet to test

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 640;
-  public static final String GIT_SHA = "7c3d1dc97ec10f36a3a1a667d77ac657b45dee51";
-  public static final String GIT_DATE = "2026-04-19 09:00:55 EDT";
-  public static final String GIT_BRANCH = "State";
-  public static final String BUILD_DATE = "2026-04-19 09:27:57 EDT";
-  public static final long BUILD_UNIX_TIME = 1776605277853L;
+  public static final int GIT_REVISION = 641;
+  public static final String GIT_SHA = "603646146be0bbcd6f5b89b571b7823cbc2d5c1c";
+  public static final String GIT_DATE = "2026-04-19 16:38:21 EDT";
+  public static final String GIT_BRANCH = "StateNewAutos";
+  public static final String BUILD_DATE = "2026-04-23 14:39:54 EDT";
+  public static final long BUILD_UNIX_TIME = 1776969594470L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
