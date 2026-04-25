77645d7eed04817484c6e5c179195801b4872607
pkatzfey <Katzfpet000@hsestudents.org>
2026-04-25 18:28:04 -0400

    auto changes 4/25

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 647;
-  public static final String GIT_SHA = "9e0249b4b595b114efb8d137b717f6cf8f697c03";
-  public static final String GIT_DATE = "2026-04-23 15:57:40 EDT";
-  public static final String GIT_BRANCH = "SimulationUpdate";
-  public static final String BUILD_DATE = "2026-04-23 17:14:22 EDT";
-  public static final long BUILD_UNIX_TIME = 1776978862233L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 651;
+  public static final String GIT_SHA = "1908d2c0d793a5f87143375581e55bed4332a49a";
+  public static final String GIT_DATE = "2026-04-25 12:51:30 EDT";
+  public static final String GIT_BRANCH = "WorldsAutos";
+  public static final String BUILD_DATE = "2026-04-25 12:56:39 EDT";
+  public static final long BUILD_UNIX_TIME = 1777136199971L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
