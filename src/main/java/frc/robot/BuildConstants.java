25e20ec7ad05b057ae871060faf6470d36ad2584
pkatzfey <Katzfpet000@hsestudents.org>
2026-04-26 13:01:25 -0400

    Merged PathPlanner changes manually

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 665;
-  public static final String GIT_SHA = "5b8d2a73c64af3f1692fb2ceba26f5f3cd653523";
-  public static final String GIT_DATE = "2026-04-25 17:22:48 EDT";
+  public static final int GIT_REVISION = 683;
+  public static final String GIT_SHA = "07a62282752c3bf9a2556f2a6a2bd1ae002b3990";
+  public static final String GIT_DATE = "2026-04-26 12:59:10 EDT";
   public static final String GIT_BRANCH = "TheGrandFinale";
-  public static final String BUILD_DATE = "2026-04-25 17:46:26 EDT";
-  public static final long BUILD_UNIX_TIME = 1777153586006L;
+  public static final String BUILD_DATE = "2026-04-26 13:00:54 EDT";
+  public static final long BUILD_UNIX_TIME = 1777222854432L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
