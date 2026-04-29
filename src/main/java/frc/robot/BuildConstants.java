34c6720e7458ef260d84c86b62a234761f16785d
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2026-04-29 14:26:12 -0400

    minor auto tweaks

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 717;
-  public static final String GIT_SHA = "f20e1ec643718404d52509a40565c64fcb3d6b08";
-  public static final String GIT_DATE = "2026-04-26 19:10:57 EDT";
-  public static final String GIT_BRANCH = "CameraLoggingUpdate";
-  public static final String BUILD_DATE = "2026-04-27 16:41:25 EDT";
-  public static final long BUILD_UNIX_TIME = 1777322485413L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 721;
+  public static final String GIT_SHA = "f4f1dad625ba7c3fb3133630f31e68a4cb8d2d04";
+  public static final String GIT_DATE = "2026-04-29 14:16:40 EDT";
+  public static final String GIT_BRANCH = "TheGrandFinale";
+  public static final String BUILD_DATE = "2026-04-29 14:17:59 EDT";
+  public static final long BUILD_UNIX_TIME = 1777486679002L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
